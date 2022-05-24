b8d37cd76dd4050a4412f74500f436709dfdef32
lihongqi1103 <105267413+lihongqi1103@users.noreply.github.com>
2022-05-24 14:59:04 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -17,7 +17,7 @@ latest_iteration = st.empty()
 bar = st.progress(0)
 
 for i in range(100):
-  latest_iteration.text(f"Iteration {i+1}")
+  latest_iteration.text(f'Progress: {i}%')
   bar.progress(i+1)
   time.sleep(0.1)
 st.balloons()
