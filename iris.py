6999e70d5d48fa482eb73f0630635892c8c905d7
lihongqi1103 <105267413+lihongqi1103@users.noreply.github.com>
2022-05-31 13:12:47 +0900

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,46 @@
+
+# from matplotlib.pyplot import text
+import matplotlib.pyplot as plt
+import sklearn
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+from sklearn import datasets
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+
+st.title("アヤメのデータ学習")
+
+st.write("学習")
+
+
+iris = datasets.load_iris()
+
+df_iris = pd.DataFrame(data = iris.data,columns=iris.feature_names)
+
+data_train, data_test, target_train, target_test = train_test_split(
+    iris.data, iris.target, test_size=0.2, random_state=0)
+
+activation_clf = st.radio(
+     "activation",
+     ('identity','logistic','tanh','relu'))
+solver_clf = st.radio(
+     "solver",
+     ('lbfgs','sgd','adam'))
+text_1 = st.number_input("中間層入力")
+text_2 = st.number_input("繰り返し回数入力")
+if st.button('開始'):
+    clf = MLPClassifier(hidden_layer_sizes=text_1,activation="activation_clf",
+                    solver="solver_clf",max_iter=text_2)
+    clf.fit(data_train,target_train)
+    st.balloons()
+    st.write("学習済み")
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.plot(clf.loss_curve_)
+    ax.legend()
+
+
