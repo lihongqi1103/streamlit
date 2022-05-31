0106ec998331293ed22aa28d6be90a1cebd682e3
lihongqi1103 <105267413+lihongqi1103@users.noreply.github.com>
2022-05-31 14:28:23 +0900

    Update iris.py

--- iris.py
+++ iris.py
@@ -10,48 +10,31 @@ from sklearn import datasets
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 
-@st.cache
-def main():
-    st.title("アヤメのデータ学習")
 
-    st.write("学習")
+st.title("アヤメのデータ学習")
+
+st.write("学習")
 
 
-    iris = datasets.load_iris()
+iris = datasets.load_iris()
 
-    df_iris = pd.DataFrame(data = iris.data,columns=iris.feature_names)
+df_iris = pd.DataFrame(data = iris.data,columns=iris.feature_names)
 
-    data_train, data_test, target_train, target_test = train_test_split(
-        iris.data, iris.target, test_size=0.2, random_state=0)
+data_train, data_test, target_train, target_test = train_test_split(
+    iris.data, iris.target, test_size=0.2, random_state=0)
 
-    activation_clf = st.radio(
-         "activation",
-         ('identity','logistic','tanh','relu'))
-    solver_clf = st.radio(
-         "solver",
-         ('sgd','adam'))
-    #text_1 = st.number_input("中間層入力")
-    text_1=st.slider("中間層入力",0,100,10,1)
-    text_2=st.slider("繰り返し回数入力",0,1000,100,1)
-    #text_2 = st.number_input("繰り返し回数入力")
-    """
-    if st.button("学習開始"):
-        clf = MLPClassifier(hidden_layer_sizes=text_1,activation= activation_clf,
-                        solver= solver_clf ,max_iter=text_2)
-        clf.fit(data_train,target_train)
-        st.balloons()
-        st.write("学習済み")
-        st.write("損失関数")
-        st.line_chart(clf.loss_curve_)
-
-        data_0 = st.slider("ガクの長さ入力",0.0,10.0,0.0,0.01)
-        data_1 = st.slider("ガクの幅入力",0.0,10.0,0.0,0.01)
-        data_2 = st.slider("花弁の長さ入力",0.0,10.0,0.0,0.01)
-        data_3 = st.slider("花弁の幅入力",0.0,10.0,0.0,0.01)
-        if st.button("識別開始"):
-            data_test=[[data_0,data_1,data_2,data_3],]
-            st.write(clf.predict(data_test))
-    """
+activation_clf = st.radio(
+     "activation",
+     ('identity','logistic','tanh','relu'))
+solver_clf = st.radio(
+     "solver",
+     ('sgd','adam'))
+#text_1 = st.number_input("中間層入力")
+text_1=st.slider("中間層入力",0,100,10,1)
+text_2=st.slider("繰り返し回数入力",0,1000,100,1)
+#text_2 = st.number_input("繰り返し回数入力")
+"""
+if st.button("学習開始"):
     clf = MLPClassifier(hidden_layer_sizes=text_1,activation= activation_clf,
                     solver= solver_clf ,max_iter=text_2)
     clf.fit(data_train,target_train)
@@ -67,4 +50,19 @@ def main():
     if st.button("識別開始"):
         data_test=[[data_0,data_1,data_2,data_3],]
         st.write(clf.predict(data_test))
-main()
+"""
+clf = MLPClassifier(hidden_layer_sizes=text_1,activation= activation_clf,
+                solver= solver_clf ,max_iter=text_2)
+clf.fit(data_train,target_train)
+st.balloons()
+st.write("学習済み")
+st.write("損失関数")
+st.line_chart(clf.loss_curve_)
+
+data_0 = st.slider("ガクの長さ入力",0.0,10.0,0.0,0.01)
+data_1 = st.slider("ガクの幅入力",0.0,10.0,0.0,0.01)
+data_2 = st.slider("花弁の長さ入力",0.0,10.0,0.0,0.01)
+data_3 = st.slider("花弁の幅入力",0.0,10.0,0.0,0.01)
+if st.button("識別開始"):
+    data_test=[[data_0,data_1,data_2,data_3],]
+    st.write(clf.predict(data_test))
