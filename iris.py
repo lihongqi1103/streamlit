278a73db3dd80ba46daee978406f31a99c416820
lihongqi1103 <105267413+lihongqi1103@users.noreply.github.com>
2022-05-31 14:35:23 +0900

    Update iris.py

--- iris.py
+++ iris.py
@@ -51,24 +51,24 @@ if st.button("学習開始"):
         data_test=[[data_0,data_1,data_2,data_3],]
         st.write(clf.predict(data_test))
 """
+if st.button("学習開始"):
+    @st.cache
+    def return_model(text_1, activation_clf, solver_clf, text_2):
+        clf = MLPClassifier(hidden_layer_sizes=text_1,activation= activation_clf,
+                        solver= solver_clf , max_iter=text_2)
+        clf.fit(data_train,target_train)
+        return clf
 
-@st.cache
-def return_model(text_1, activation_clf, solver_clf, text_2):
-    clf = MLPClassifier(hidden_layer_sizes=text_1,activation= activation_clf,
-                    solver= solver_clf , max_iter=text_2)
-    clf.fit(data_train,target_train)
-    return clf
+    clf = return_model(text_1, activation_clf, solver_clf, text_2)
+    st.balloons()
+    st.write("学習済み")
+    st.write("損失関数")
+    st.line_chart(clf.loss_curve_)
 
-clf = return_model(text_1, activation_clf, solver_clf, text_2)
-st.balloons()
-st.write("学習済み")
-st.write("損失関数")
-st.line_chart(clf.loss_curve_)
-
-data_0 = st.slider("ガクの長さ入力",0.0,10.0,0.0,0.01)
-data_1 = st.slider("ガクの幅入力",0.0,10.0,0.0,0.01)
-data_2 = st.slider("花弁の長さ入力",0.0,10.0,0.0,0.01)
-data_3 = st.slider("花弁の幅入力",0.0,10.0,0.0,0.01)
-if st.button("識別開始"):
-    data_test=[[data_0,data_1,data_2,data_3],]
-    st.write(clf.predict(data_test))
+    data_0 = st.slider("ガクの長さ入力",0.0,10.0,0.0,0.01)
+    data_1 = st.slider("ガクの幅入力",0.0,10.0,0.0,0.01)
+    data_2 = st.slider("花弁の長さ入力",0.0,10.0,0.0,0.01)
+    data_3 = st.slider("花弁の幅入力",0.0,10.0,0.0,0.01)
+    if st.button("識別開始"):
+        data_test=[[data_0,data_1,data_2,data_3],]
+        st.write(clf.predict(data_test))
