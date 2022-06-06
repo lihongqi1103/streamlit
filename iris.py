e00a358f9c8adca8853bb9747818338b8ea292f0
lihongqi1103 <105267413+lihongqi1103@users.noreply.github.com>
2022-06-06 18:58:50 +0900

    Update iris.py

--- iris.py
+++ iris.py
@@ -58,7 +58,6 @@ if start=="YES":
                         solver= solver_clf , max_iter=text_2)
         clf.fit(data_train,target_train)
         st.balloons()
-        @st.cache(suppress_st_warning=True)
         return clf
 
     clf = return_model(text_1, activation_clf, solver_clf, text_2)
