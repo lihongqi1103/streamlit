58e5ed404e3dd1678be05163e9709d460962d370
lihongqi1103 <105267413+lihongqi1103@users.noreply.github.com>
2022-06-07 15:06:46 +0900

    Update iris.py

--- iris.py
+++ iris.py
@@ -52,12 +52,11 @@ start = st.radio(
      "学習開始",
      ('NO','YES'))
 if start=="YES":
-    
+    @st.cache
     def return_model(text_1, activation_clf, solver_clf, text_2):
         clf = MLPClassifier(hidden_layer_sizes=text_1,activation= activation_clf,
                         solver= solver_clf , max_iter=text_2)
         clf.fit(data_train,target_train)
-        st.balloons()
         return clf
 
     clf = return_model(text_1, activation_clf, solver_clf, text_2)
